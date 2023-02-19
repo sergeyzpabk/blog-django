900a80b07d0c10793da4c9dcc0a4be9c128bcf9c
sergeyatero@gmail.com <Tools123456>
2023-02-20 06:39:08 +0700

    Start Project

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст записи')
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+    img = models.ImageField('Изображение', upload_to='image/%Y')
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+class Comments(models.Model):
+    email = models.EmailField()
+    name = models.CharField('Имя', max_length=50)
+    text_comments = models.TextField('Текст комментария', max_length=2000)
+    post = models.ForeignKey(Post, verbose_name='Публикация', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}, {self.post}'
+
+    class Meta:
+        verbose_name = 'Коментарий'
+        verbose_name_plural = 'Коментарии'
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,10 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,8 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [STATIC_DIR]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
